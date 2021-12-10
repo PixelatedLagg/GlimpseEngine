2e1f5d8c30ca2d482a807d17f770601691387466
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-12-09 20:43:02 -0500

    cleaned up code

--- HeadshotEngine.Editor/Editor/MainWindow.axaml.cs
+++ HeadshotEngine.Editor/Editor/MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System.Net.Http.Headers;
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace Editor
 {
@@ -10,9 +14,34 @@ namespace Editor
         {
             InitializeComponent();
         }
-        private void InitializeComponent()
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        void TestClick(object sender, RoutedEventArgs args)
         {
-            AvaloniaXamlLoader.Load(this);
+            var b = (Button)sender;
+            if (b.Content.ToString() == "aids")
+            {
+                b.Content = "not aids";
+            }
+            else
+            {
+                b.Content = "aids";
+            }
         }
+        /*void PositionChange(object sender, RoutedEventArgs args)
+        {
+            TextBlock? text = this.FindControl<TextBlock>("aids") as TextBlock;
+            if (text != null)
+            {
+                text.Text = "test";
+            }
+        }
+        void Move(object sender, EventRoute args)
+        {
+            Panel? panel = sender as Panel;
+            if (panel != null)
+            {
+                panel.Background = new SolidColorBrush(Color.FromArgb(100, 2, 3, 2));
+            }
+        }*/
     }
 }
