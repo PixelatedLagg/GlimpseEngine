30361407062d78244f3d8f00a81a0d35d4d06efd
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-03-10 23:57:05 -0500

    nearly got glfw to work (nearly)

--- all-src/new-src/main.c
+++ all-src/new-src/main.c
@@ -9,17 +9,6 @@
 
 int main()
 {
-    /*V3 v = NewV3(0.0F, 0.0F, 0.0F);
-    GameObject o = NewGObject("e", NewV3(0.0F, 0.0F, 0.0F), NewV3(2.0F, 2.0F, 2.0F), NewV3(0.0F, 0.0F, 0.0F));
-    GameObject b = NewGObject("t", NewV3(2.0F, 0.0F, 0.0F), NewV3(1.0F, 1.0F, 1.0F), NewV3(0.0F, 0.0F, 0.0F));
-    if (IsColliding(o, b))
-    {
-        printf("collided\n");
-    }
-    else
-    {
-        printf("did not collide\n");
-    }*/
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -27,7 +16,7 @@ int main()
     GLFWwindow* window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
     if (window == NULL)
     {
-        printf("Failed to create GLFW window");
+        printf("Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
     }
